86b3fe9e797f5b9d96b28d25e3708867e7e17152
Hin Wong <thwong.henry@gmail.com>
2020-05-07 00:10:53 +0800

    feat: handle chat event observer when player state change

--- src/content-script/live-chat-overlay/app.tsx
+++ src/content-script/live-chat-overlay/app.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import { useChatEventObserver } from './hooks/use-chat-event-observer';
+import { useInitChatEventObserver } from './hooks/use-init-chat-event-observer';
 import { useResetChatEventsOnPlayerRectChange } from './hooks/use-reset-chat-events-on-player-rect-change';
+import { useHandleChatEventOnPlayerStateChange } from './hooks/use-handle-chat-event-on-player-state-change';
 
 import ChatFlow from './components/chat-flow';
 
 export default function App(): JSX.Element {
-    useChatEventObserver();
+    useInitChatEventObserver();
     useResetChatEventsOnPlayerRectChange();
+    useHandleChatEventOnPlayerStateChange();
 
     return <ChatFlow />;
 }
