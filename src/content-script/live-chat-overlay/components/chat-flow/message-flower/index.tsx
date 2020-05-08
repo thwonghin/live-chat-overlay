45e40b37d5cc7adebb12d0072a0f4deadf26e34c
Hin Wong <thwong.henry@gmail.com>
2020-05-09 01:09:03 +0800

    perf: remove will change property

--- src/content-script/live-chat-overlay/components/chat-flow/message-flower/index.tsx
+++ src/content-script/live-chat-overlay/components/chat-flow/message-flower/index.tsx
@@ -40,7 +40,6 @@ const MessageFlower: React.FC<Props> = ({ children, chatItem, onTimeout }) => {
             left: containerWidth || 99999,
             top: chatItem.position.lineNumber * lineHeight,
             fontSize: lineHeight,
-            willChange: 'transform',
             transform: isFlowing
                 ? `translate3d(-${containerWidth + rect.width}px, 0, 0)`
                 : 'translate3d(0, 0, 0)',
