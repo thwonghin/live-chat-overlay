83b794cdc58776f3b63ed123c36e5de285b791dd
Hin Wong <thwong.henry@gmail.com>
2020-05-09 01:03:38 +0800

    perf: use hw accel

--- src/content-script/live-chat-overlay/components/chat-flow/message-flower/index.tsx
+++ src/content-script/live-chat-overlay/components/chat-flow/message-flower/index.tsx
@@ -40,9 +40,10 @@ const MessageFlower: React.FC<Props> = ({ children, chatItem, onTimeout }) => {
             left: containerWidth || 99999,
             top: chatItem.position.lineNumber * lineHeight,
             fontSize: lineHeight,
+            willChange: 'transform',
             transform: isFlowing
-                ? `translateX(-${containerWidth + rect.width}px)`
-                : 'translateX(0)',
+                ? `translate3d(-${containerWidth + rect.width}px, 0, 0)`
+                : 'translate3d(0, 0, 0)',
         }),
         [
             timeout,
