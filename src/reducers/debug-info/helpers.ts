13428fd6e44fb2557e7354d17673f796c76a33c4
Hin Wong <thwong.henry@gmail.com>
2020-06-12 23:12:05 +0800

    feat: reducer for debug info

---
+++ src/reducers/debug-info/helpers.ts
@@ -0,0 +1,14 @@
+import { Benchmark } from './types';
+
+export function calculateBenchmark(
+    oldBenchmark: Benchmark,
+    newValue: number,
+): Benchmark {
+    const newCount = oldBenchmark.count + 1;
+    return {
+        min: Math.min(oldBenchmark.min, newValue),
+        max: Math.max(oldBenchmark.max, newValue),
+        avg: (oldBenchmark.avg * oldBenchmark.count + newValue) / newCount,
+        count: newCount,
+    };
+}
---
+++ src/reducers/debug-info/index.ts
@@ -0,0 +1,71 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { State } from './types';
+import { calculateBenchmark } from './helpers';
+
+const initialState: State = {
+    isDebugging: false,
+    processXhrBenchmark: {
+        min: Number.MAX_SAFE_INTEGER,
+        max: 0,
+        avg: 0,
+        count: 0,
+    },
+    processChatEventBenchmark: {
+        min: Number.MAX_SAFE_INTEGER,
+        max: 0,
+        avg: 0,
+        count: 0,
+    },
+};
+
+const debugInfoSlice = createSlice({
+    name: 'debug-info',
+    initialState,
+    reducers: {
+        startDebug(state) {
+            return {
+                ...state,
+                isDebugging: true,
+            };
+        },
+        stopDebug(state) {
+            return {
+                ...state,
+                isDebugging: false,
+            };
+        },
+        addProcessXhrMetric(state, action: PayloadAction<number>) {
+            return {
+                ...state,
+                processXhrBenchmark: calculateBenchmark(
+                    state.processXhrBenchmark,
+                    action.payload * 1000,
+                ),
+            };
+        },
+        addProcessChatEventMetric(state, action: PayloadAction<number>) {
+            return {
+                ...state,
+                processChatEventBenchmark: calculateBenchmark(
+                    state.processChatEventBenchmark,
+                    action.payload * 1000,
+                ),
+            };
+        },
+        resetMetrics(state) {
+            return {
+                ...initialState,
+                isDebugging: state.isDebugging,
+            };
+        },
+        reset() {
+            return {
+                ...initialState,
+            };
+        },
+    },
+});
+
+export const debugInfoActions = debugInfoSlice.actions;
+export const debugInfoReducer = debugInfoSlice.reducer;
