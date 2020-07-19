a7918ec44be8093a5e2240d315b10204784d0dee
Hin Wong <thwong.henry@gmail.com>
2020-07-19 19:59:12 +0800

    fix: overlapping two line message

--- src/reducers/chat-events/helpers.ts
+++ src/reducers/chat-events/helpers.ts
@@ -1,55 +1,90 @@
 import { last } from 'lodash-es';
 
-import { MessageSettings } from '@/services/settings/types';
-import { State } from './types';
+import type { UiChatItem } from '@/components/chat-flow/types';
 
-interface GetPositionParams {
-    state: State;
+interface HasSpaceInLineParams {
+    chatItemsByLineNumber: Record<number, UiChatItem[]>;
     estimatedMsgWidth: number;
-    messageSettings: MessageSettings;
+    addTimestamp: number;
+    flowTimeInSec: number;
+    containerWidth: number;
+    charWidth: number;
+    lineNumber: number;
+}
+
+function hasSpaceInLine({
+    chatItemsByLineNumber,
+    estimatedMsgWidth,
+    addTimestamp,
+    flowTimeInSec,
+    containerWidth,
+    charWidth,
+    lineNumber,
+}: HasSpaceInLineParams): boolean {
+    const messages = chatItemsByLineNumber[lineNumber];
+
+    if (!messages || messages.length === 0) {
+        return true;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const lastMessage = last(messages)!;
+
+    const lastMsgFlowedTime = (addTimestamp - lastMessage?.addTimestamp) / 1000;
+    const lastMsgWidth = lastMessage?.estimatedMsgWidth * charWidth;
+    const lastMsgSpeed = (containerWidth + lastMsgWidth) / flowTimeInSec;
+    const lastMsgPos = lastMsgSpeed * lastMsgFlowedTime - lastMsgWidth;
+
+    const remainingTime = flowTimeInSec - lastMsgFlowedTime;
+
+    const estimatedEleWidth = estimatedMsgWidth * charWidth;
+    const speed = (containerWidth + estimatedEleWidth) / flowTimeInSec;
+
+    return speed * remainingTime < containerWidth && lastMsgPos > 0;
+}
+
+interface GetLineNumberParams {
+    chatItemsByLineNumber: Record<number, UiChatItem[]>;
+    estimatedMsgWidth: number;
     maxLineNumber: number;
     addTimestamp: number;
     flowTimeInSec: number;
     containerWidth: number;
     charWidth: number;
+    displayNumberOfLines: number;
 }
 
 export function getLineNumber({
-    state,
+    chatItemsByLineNumber,
     estimatedMsgWidth,
-    messageSettings,
     maxLineNumber,
     addTimestamp,
     flowTimeInSec,
     containerWidth,
     charWidth,
-}: GetPositionParams): number | null {
+    displayNumberOfLines,
+}: GetLineNumberParams): number | null {
     for (
         let lineNumber = 0;
-        lineNumber < maxLineNumber - messageSettings.numberOfLines - 1;
+        lineNumber < maxLineNumber - displayNumberOfLines - 1;
         lineNumber += 1
     ) {
-        const messages = state.chatItemsByLineNumber[lineNumber];
-
-        if (!messages || messages.length === 0) {
-            return lineNumber;
-        }
-
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const lastMessage = last(messages)!;
-
-        const lastMsgFlowedTime =
-            (addTimestamp - lastMessage?.addTimestamp) / 1000;
-        const lastMsgWidth = lastMessage?.estimatedMsgWidth * charWidth;
-        const lastMsgSpeed = (containerWidth + lastMsgWidth) / flowTimeInSec;
-        const lastMsgPos = lastMsgSpeed * lastMsgFlowedTime - lastMsgWidth;
-
-        const remainingTime = flowTimeInSec - lastMsgFlowedTime;
-
-        const estimatedEleWidth = estimatedMsgWidth * charWidth;
-        const speed = (containerWidth + estimatedEleWidth) / flowTimeInSec;
-
-        if (speed * remainingTime < containerWidth && lastMsgPos > 0) {
+        if (
+            Array(displayNumberOfLines)
+                .fill(null)
+                .map((v, index) => index + lineNumber)
+                .every((loopLineNumber) =>
+                    hasSpaceInLine({
+                        chatItemsByLineNumber,
+                        estimatedMsgWidth,
+                        addTimestamp,
+                        flowTimeInSec,
+                        containerWidth,
+                        charWidth,
+                        lineNumber: loopLineNumber,
+                    }),
+                )
+        ) {
             return lineNumber;
         }
     }
