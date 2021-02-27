050296a8737b11d3121b4f0c479c48dce8c05a5b
Hin Wong <13003836+thwonghin@users.noreply.github.com>
2021-02-27 16:56:55 +0800

    chore: remove unused option'

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,7 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     parserOptions: {
         project: './tsconfig.json',
-        tsconfigRootDir: __dirname,
-        ecmaVersion: 2020,
-        sourceType: "module"
+        tsconfigRootDir: __dirname
     },
     rules: {
         "react/jsx-uses-react": "off",
--- webpack/src/types/xo-webpack-plugin.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'xo-webpack-plugin';
