42c91f9f697d4135b10e45c23474e865646e0be4
Hin Wong <13003836+thwonghin@users.noreply.github.com>
2023-06-01 00:05:08 -0400

    chore: use vite instead of webpack

--- src/content-script.ts
+++ src/content-script.ts
@@ -8,10 +8,10 @@ import { injectLiveChatOverlay } from './app/live-chat-overlay';
 
 async function init(): Promise<void> {
     await settingsStorage.storageInstance.init(browser);
-    injectScript(browser.runtime.getURL('live-chat-fetch-interceptor.js'));
+    injectScript(browser.runtime.getURL('src/live-chat-fetch-interceptor.js'));
 
     const initData = await youtube.getInitData(
-        browser.runtime.getURL('get-live-chat-init-data.js'),
+        browser.runtime.getURL('src/get-live-chat-init-data.js'),
     );
 
     await youtube.waitForPlayerReady();
