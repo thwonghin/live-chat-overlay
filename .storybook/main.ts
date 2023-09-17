1f7e02bd527c9f7843c75847cc9723d116804da8
Hin Wong <13003836+thwonghin@users.noreply.github.com>
2023-09-17 01:14:17 -0400

    chore: remove unused packages (#778)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,6 @@ const config: StorybookConfig = {
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
-        'storybook-addon-swc',
     ],
     framework: {
         name: '@storybook/html-vite',
