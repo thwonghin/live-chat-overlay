86aecdf369caa5cc87304902213269480bdd4852
Hin Wong <13003836+thwonghin@users.noreply.github.com>
2023-10-30 21:07:17 -0400

    fix: cleanup chat when loading into non live video (#814)

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
 export const CHAT_START_EVENT = 'chat-start';
-export const CHAT_END_EVENT = 'chat-end';
 export const LIVE_CHAT_API_INTERCEPT_EVENT = 'live-chat-api-intercept';
 export const LIVE_CHAT_INIT_DATA = 'live-chat-init-data';
 export const HAS_LIVE_CHAT = 'has-live-chat';
